9dcf86f150aefdaa1629038c0986ddd36c2673bd
dlwlsgur0909 <dlwlsgur0909@naver.com>
2023-03-06 21:32:15 +0900

    숫자야구 template 수정, data 추가 onSubmitForm 메소드 수정

--- 3. 숫자야구/NumberBaseball.vue
+++ 3. 숫자야구/NumberBaseball.vue
@@ -1,11 +1,17 @@
 <template>
     <div>
         <h1>{{result}}</h1>
-        <form v-on:submit="onSubmitForm">
-            <input ref="answer" maxlength="4" v-model="value">
-            <button>입력</button>
+        <form @submit.prevent="onSubmitForm">
+            <input ref="answer" minlength="4" v-oncemaxlength="4" v-model="input">
+            <button type="submit">입력</button>
         </form>
-        <div>시도: {{  }}</div>
+        <div>시도: {{ tries.length }}</div>
+        <ul>
+            <li v-for="t in tries">
+                <div>{{ t.try }}</div>
+                <div>{{ t.result }}</div>
+            </li>
+        </ul>
     </div>
 </template>
 
@@ -13,13 +19,19 @@
     export default {
         data() {
             return {
-                value: '',
+                tries: [],
+                input: '',
                 result: '',
             }
         },
         methods: {
-            onSubmitForm(e) {
-                e.preventDefault();
+            onSubmitForm() {
+                this.tries.push({
+                    try: this.input,
+                    result: '홈런',
+                });
+                this.input = '';
+                this.$refs.answer.focus();
             }
         },
     };
