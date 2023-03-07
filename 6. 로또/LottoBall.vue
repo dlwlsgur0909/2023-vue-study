68b1d9c878cb3d92fc853cc9febd59a16e3a17fa
dlwlsgur0909 <dlwlsgur0909@naver.com>
2023-03-07 22:06:22 +0900

    로또 추첨 구현

--- 6. 로또/LottoBall.vue
+++ 6. 로또/LottoBall.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="ball" :style="{ background }"> {{number}}</div>
+    <div class="ball" :style="styleObject"> {{number}}</div>
 
 </template>
 
@@ -7,12 +7,27 @@
 
     export default {
         name: 'LottoBall',
-        props: ['number'],
-        data() {
-            let background;
-            return {
-                background: '',
-            };
+        props: {
+            number: Number,
+        },
+        computed: {
+            styleObject() {
+                let background;
+                if(this.number <= 10) {
+                    background = 'red';
+                }else if(this.number <= 20) {
+                    background = 'orange';
+                }else if(this.number <= 30) {
+                    background = 'yellow';
+                }else if(this.number <= 40) {
+                    background = 'blue';
+                }else {
+                    background = 'green';
+                }
+                return {
+                    background,
+                };
+            },
         },
     }
 
--- 6. 로또/LottoGenerator.vue
+++ 6. 로또/LottoGenerator.vue
@@ -2,11 +2,11 @@
     <div>
         <div>당첨숫자</div>
         <div id="result-area">
-            <lotto-ball v-for="ball in winballs" number="5"></lotto-ball>
+            <lotto-ball v-for="ball in winBalls" v-bind:number="ball" v-bind:key="ball"></lotto-ball>
         </div>
         <div>보너스!</div>
-        <lotto-ball v-if="bonus"></lotto-ball>
-        <button v-if="redo">한번 더!</button>
+        <lotto-ball v-if="bonus" :number="bonus"></lotto-ball>
+        <button v-if="redo" @click="onClickRedo">한번 더!</button>
     </div>
     
 </template>
@@ -26,6 +26,8 @@
         return [...winNumbers, bonusNumber]
     }
 
+    const timeouts = [];
+
     export default {
         components: {
             'lotto-ball': LottoBall, // PascalCase는 vue에서 자동으로 kebab-case로 변환 가능하므로 그냥 LottoBall만 적어도 된다.
@@ -39,7 +41,27 @@
             }
         },
         methods: {
-           
+           onClickRedo() {
+            this.winNumbers = getWinNumbers();
+            this.winBalls = [];
+            this.bonus = null;
+            this.redo = false;
+            this.showBalls();
+           },
+           showBalls() {
+                // 당첨 번호 출력
+                for(let i=0; i<this.winNumbers.length - 1; i++) {
+                    timeouts[i] = setTimeout(() => {
+                        this.winBalls.push(this.winNumbers[i]);
+                    }, (i + 1) * 1000);
+                }
+
+                // 보너스 번호 출력
+                timeouts[6] = setTimeout(() => {
+                    this.bonus = this.winNumbers[6];
+                    this.redo = true;
+                }, 7000);
+           }
         },
         
         computed: {
@@ -47,10 +69,12 @@
            
         },
         mounted() {
-
+           this.showBalls();
         },
         beforeDestroy() {
-
+            timeouts.forEach((t) => {
+                clearTimeout(t)
+            });
         },
         watch: {
 
