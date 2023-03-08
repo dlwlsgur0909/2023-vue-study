9ed62145b8f340868b86d260a859d64d64fc5c36
dlwlsgur0909 <dlwlsgur0909@naver.com>
2023-03-08 14:30:48 +0900

    vuex install, store.js 생성

---
+++ 7. 틱택토Vuex/store.js
@@ -0,0 +1,16 @@
+import Vuex from 'vuex';
+
+export default new Vuex.Store({
+    state: {
+
+    }, // vue의 data와 비슷
+    getters: {
+
+    }, // vue의 computed와 비슷
+    mutations: {
+
+    }, // state를 수정할 때 사용 (동기적)
+    actions: {
+
+    }, // 비동기 혹은 여러 mutation을 연달아 실행할 때 사용
+});
